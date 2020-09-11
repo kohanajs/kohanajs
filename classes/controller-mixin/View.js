13a32d546d69a708c8a300b5bf17641888052af1
Kojin Nakana <kohanajs@outlook.com>
2020-09-11 14:32:47 +0800

    render errorTemplate

--- classes/controller-mixin/View.js
+++ classes/controller-mixin/View.js
@@ -1,16 +1,22 @@
 const {ControllerMixin, View} = require("@kohanajs/core-mvc");
 
 class ControllerMixinView extends ControllerMixin{
+  /** @type {View} */
+  template = null;
+  /** @type {View} */
+  errorTemplate = null;
+
   constructor(client, layout='layout/default', themePath= null, viewClass = View.defaultViewClass) {
     super(client);
 
     this.layout = this.getView(layout, {}, themePath, viewClass);
 
     this.exports = {
-      getView     : (file, data= {}) => this.getView(file, data, themePath, viewClass),
-      setTemplate : (file, data= {}) => (this.template = this.getView(file, data, themePath, viewClass)),
-      template    : () => this.template,
-      layout      : () => this.layout,
+      getView          : (file, data= {}) => this.getView(file, data, themePath, viewClass),
+      setTemplate      : (file, data= {}) => (this.template = this.getView(file, data, themePath, viewClass)),
+      setErrorTemplate : (file, data= {}) => (this.errorTemplate = this.getView(file, data, themePath, viewClass)),
+      template         : () => this.template,
+      layout           : () => this.layout,
     }
   }
 
@@ -18,6 +24,13 @@ class ControllerMixinView extends ControllerMixin{
     //render template and put into layout's main output.
     //no template, replace the controller body string into layout.
     this.layout.data.main = (this.template) ? await this.template.render() : this.client.body;
+    this.client.body = await this.layout.render();
+  }
+
+  async exit(code){
+    if(code!==404 || code !== 403 || code !== 500)return;
+
+    (this.errorTemplate) ? await this.errorTemplate.render() : this.client.body
     this.client.body = await this.layout.render();
   }
 
